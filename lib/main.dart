55cef30c5b0e2634b667f9136bf2b4a254424135
FelienZ <rajabfadh@gmail.com>
2025-10-27 12:51:07 +0700

    delete unused page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => Homepage(),
-        '/wisata': (context) => Wisatapage(),
+        '/wisatapage': (context) => Wisatapage(),
       },
     );
   }
